9acead50860b32375607feaeec24aad6b2961eae
riswan29 <wandra290401@gmail.com>
2022-12-12 20:16:54 +0700

    add route for explore, index and open cam detection

--- app.py
+++ app.py
@@ -62,23 +62,12 @@ def gen_frames():  # generate frame by frame from camera
                 confidences = []
                 classIDs = []
                 centers = []
-                # loop over each of the layer outputs
                 for output in layerOutputs:
-                    # loop over each of the detections
                     for detection in output:
-                        # extract the class ID and confidence (i.e., probability) of
-                        # the current object detection
                         scores = detection[5:]
                         classID = np.argmax(scores)
                         confidence = scores[classID]
-
-                        # filter out weak predictions by ensuring the detected
-                        # probability is greater than the minimum probability
                         if confidence > 0.5:
-                            # scale the bounding box coordinates back relative to the
-                            # size of the image, keeping in mind that YOLO actually
-                            # returns the center (x, y)-coordinates of the bounding
-                            # box followed by the boxes' width and height
                             box = detection[0:4] * np.array([W, H, W, H])
                             (centerX, centerY, width, height) = box.astype("int")
 
@@ -93,13 +82,7 @@ def gen_frames():  # generate frame by frame from camera
                             confidences.append(float(confidence))
                             classIDs.append(classID)
                             centers.append((centerX, centerY))
-                        # yield (b'--frame\r\n'
-                        # b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-                # apply non-maxima suppression to suppress weak, overlapping bounding
-                # boxes
                 idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
-                #draw bounding boxes on the image
                 colors= np.random.uniform(0,255, size=(len(LABELS),3))
                 for i in idxs:
                     i=i
@@ -109,15 +92,11 @@ def gen_frames():  # generate frame by frame from camera
                     cv2.rectangle(frame, (round(x),round(y)), (round(x+w), round(y+h)),color,2)
                     label="%s: %.2f" %(LABELS[classID], confidences[i])
                     cv2.putText(frame, label, (x-10,y-10),cv2.FONT_HERSHEY_SIMPLEX,1, color,2)
-                cv2.imshow("Object detection",frame)
                 cv2.waitKey(50)
                 texts = []
 
-                # ensure at least one detection exists
                 if len(idxs) > 0:
-                    # loop over the indexes we are keeping
                     for i in idxs.flatten():
-                        # find positions
                         centerX, centerY = centers[i][0], centers[i][1]
                         if centerX <= W/3:
                             W_pos = "kiri "
@@ -140,47 +119,24 @@ def gen_frames():  # generate frame by frame from camera
                     tts = gTTS(description, lang='id')
                     tts.save('tts.mp3')
                     ipd.Audio('tts.mp3')
-                    #tts = AudioSegment.from_mp3("tts.mp3")
                     subprocess.call(["ffplay", "-nodisp", "-autoexit", "tts.mp3"])
-    # os.remove("tts.mp3")
 
                 
-                #     # font which we will be using to display FPS
-                # font = cv2.FONT_HERSHEY_SIMPLEX
 
-                #     # time when we finish processing for this frame
-                # new_frame_time = time.time()
 
-                # fps = 1 / (new_frame_time - prev_frame_time)
-                # prev_frame_time = new_frame_time
-
-                #     # converting the fps into integer
-                # fps = int(round(fps))
-
-                #     # converting the fps to string so that we can display it on frame
-                #     # by using putText function
-                # fps = str(fps)
-
-                #     # puting the FPS count on the frame
-                #     cv2.putText(frame, fps, (550, 50), font, 2, (100, 255, 0), 3, cv2.LINE_AA)
-
-                #     # Show the result
-                #     cv2.imshow('Result', frame)
-
-                #     if cv2.waitKey(5) & 0xFF == 27:
-                #         break
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+                        b'Content-Type: image/jpeg\r\n\r\n' +frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/eksplore')
+def eksplore():
+    return render_template('explore.html')
 
 @app.route('/')
 def index():
